4d5bdd6908f75d35e08e12afd03a9a69a1488ad3
Joey J <joey@wyxos.com>
2025-01-04 20:31:53 +0400

    list databases

--- config/host.php
+++ config/host.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-  'server' => [
-      'db' => [
-          'username' => env('SERVER_DB_USERNAME'),
-          'password' => env('SERVER_DB_PASSWORD')
-      ]
-  ]
+    'db' => [
+        'username' => env('SERVER_DB_USERNAME'),
+        'password' => env('SERVER_DB_PASSWORD')
+    ]
 ];
