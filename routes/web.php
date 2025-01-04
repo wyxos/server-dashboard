3e2871f10ee82160ba69572e8c89e4643c93971f
Joey J <joey@wyxos.com>
2025-01-04 21:13:23 +0400

    list databases

--- routes/web.php
+++ routes/web.php
@@ -2,36 +2,58 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\DB;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-use Illuminate\Support\Facades\DB;
-
 Route::get('/databases', function () {
     try {
-        // Run the command to list databases
+        // Run the command to list databases and their associated users
         $output = [];
         $exitCode = null;
 
         $username = config('host.db.username');
         $password = config('host.db.password');
 
+        // Query to fetch databases and their associated users
+        $query = "
+            SELECT db.Db AS database_name, user.User AS user_name
+            FROM mysql.db AS db
+            INNER JOIN mysql.user AS user ON db.User = user.User
+            ORDER BY database_name, user_name;
+        ";
+
         // Execute the MariaDB command
-        exec("mysql -u $username -p$password -e 'SHOW DATABASES;' 2>&1", $output, $exitCode);
+        exec("mysql -u $username -p$password -e \"$query\" 2>&1", $output, $exitCode);
 
         // Check if the command was successful
         if ($exitCode !== 0) {
             return response()->json([
-                'error' => 'Failed to fetch databases',
-                'details' => implode("\n", $output)
+                'error' => 'Failed to fetch databases and users',
+                'details' => implode("\n", $output),
             ], 500);
         }
 
-        // Return the list of databases as a JSON response
-        return response()->json($output);
+        // Parse the output
+        $parsedOutput = [];
+        $header = null;
+
+        foreach ($output as $line) {
+            $columns = preg_split('/\s+/', $line);
+            if (!$header) {
+                $header = $columns;
+                continue;
+            }
+            $parsedOutput[] = array_combine($header, $columns);
+        }
+
+        // Return the parsed output as JSON
+        return response()->json($parsedOutput);
     } catch (\Exception $e) {
         // Handle any errors
         return response()->json(['error' => $e->getMessage()], 500);
     }
 });
+
